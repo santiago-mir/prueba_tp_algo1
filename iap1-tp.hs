d10d0c46dbbc65120f9ce094b60259b52a316724
Santiago <san_chan97@hotmail.com>
2023-05-10 23:15:04 -0300

    update ej 1 al 9

--- iap1-tp.hs
+++ iap1-tp.hs
@@ -16,7 +16,7 @@ usuario2 = (2, "Natalia")
 usuario3 = (3, "Pedro")
 usuario4 = (4, "Mariela")
 usuario5 = (5, "Natalia")
-usuario6 = (6, "Pepito")
+
 
 
 relacion1_2 = (usuario1, usuario2)
@@ -25,7 +25,10 @@ relacion1_4 = (usuario4, usuario1) -- Notar que el orden en el que aparecen los 
 relacion2_3 = (usuario3, usuario2)
 relacion2_4 = (usuario2, usuario4)
 relacion3_4 = (usuario4, usuario3)
-relacion4_6 = (usuario4, usuario6)
+relacion4_5 = (usuario4, usuario5)
+
+
+
 
 publicacion1_1 = (usuario1, "Este es mi primer post", [usuario2, usuario4])
 publicacion1_2 = (usuario1, "Este es mi segundo post", [usuario4])
@@ -34,6 +37,7 @@ publicacion1_4 = (usuario1, "Este es mi cuarto post", [])
 publicacion1_5 = (usuario1, "Este es como mi quinto post", [usuario5])
 
 publicacion2_1 = (usuario2, "Hello World", [usuario4])
+publicacion2_1i = (usuario2, "Hello World", [usuario4])
 publicacion2_2 = (usuario2, "Good Bye World", [usuario1, usuario4])
 
 publicacion3_1 = (usuario3, "Lorem Ipsum", [])
@@ -46,12 +50,12 @@ publicacion4_3 = (usuario4, "Just kidding, i am Mariela", [usuario1, usuario3])
 
 
 usuariosA = [usuario1, usuario2, usuario3, usuario4]
-relacionesA = [relacion1_2, relacion1_4, relacion2_3, relacion2_4, relacion3_4, relacion4_6]
-publicacionesA = [publicacion1_1, publicacion1_2, publicacion2_1, publicacion2_2, publicacion3_1, publicacion3_2, publicacion4_1, publicacion4_2]
+relacionesA = [relacion1_2, relacion1_4, relacion2_3, relacion2_4, relacion3_4]
+publicacionesA = [publicacion1_1, publicacion1_2, publicacion2_1, publicacion2_2, publicacion3_1, publicacion3_2, publicacion4_1, publicacion4_2, publicacion2_1i]
 redA = (usuariosA, relacionesA, publicacionesA)
 
 usuariosB = [usuario1, usuario2, usuario3, usuario5]
-relacionesB = [relacion1_2, relacion2_3]
+relacionesB = [relacion1_2, relacion2_3, relacion4_5 ]
 publicacionesB = [publicacion1_3, publicacion1_4, publicacion1_5, publicacion3_1, publicacion3_2, publicacion3_3]
 redB = (usuariosB, relacionesB, publicacionesB)
 
@@ -82,6 +86,7 @@ likesDePublicacion (_, _, us) = us
 
 -- Ejercicios
 
+
 longitud :: [t] -> Integer
 longitud [] = 0
 longitud (x:xs) = 1 + longitud xs
@@ -89,6 +94,9 @@ longitud (x:xs) = 1 + longitud xs
 pertenece :: (Eq t) => t -> [t] -> Bool
 pertenece _ [] = False
 pertenece x (y:ys) = x == y || pertenece x ys
+
+
+-- Recorre la lista de usuarios de la red social y toma los nombres de cada usuario, sin tener en cuenta usuarios repetidos
 
 nombresDeUsuarios :: RedSocial -> [String]
 nombresDeUsuarios ((u:us), rs, ps) | longitud us == 0 = [nombreDeUsuario u]
@@ -105,12 +113,20 @@ amigosDe (u, (r:rs), ps) user | longitud rs == 0 && not (estaEnUnaRelacion r use
                               | otherwise                                          = amigosDe (u, rs, ps) user
 
 estaEnUnaRelacion :: Relacion -> Usuario -> Bool
-estaEnUnaRelacion (u1, u2) user = idDeUsuario u1 == idDeUsuario user || idDeUsuario u2 == idDeUsuario user
+estaEnUnaRelacion (u1, u2) user = u1 == user || u2 == user
 
 userRelacionado :: Relacion -> Usuario -> Usuario
-userRelacionado (u1, u2) user | idDeUsuario u1 == idDeUsuario user = u2
-                              | idDeUsuario u2 == idDeUsuario user = u1
+userRelacionado (u1, u2) user | u1 == user = u2
+                              | u2 == user = u1
 
+esLaMismaRelacion :: Relacion -> Relacion -> Bool
+esLaMismaRelacion r1 r2 | (fst r1 == fst r2 && snd r1 == snd r2) || (fst r1 == snd r2 && snd r1 == fst r2) = True
+                        | otherwise                                                                        = False
+
+laRelacionPertenece :: Relacion -> [Relacion] -> Bool
+laRelacionPertenece rel (r:rs) | longitud rs == 0 && not (esLaMismaRelacion r rel) = False
+                               | esLaMismaRelacion r rel = True
+                               | not (esLaMismaRelacion r rel) = laRelacionPertenece rel rs                       
 -- describir qué hace la función: .....
 cantidadDeAmigos :: RedSocial -> Usuario -> Int
 cantidadDeAmigos rs us = fromIntegral (longitud (amigosDe rs us))
@@ -149,16 +165,40 @@ publicacionesDe (us, rs, (p:ps)) user | longitud ps == 0 && usuarioDePublicacion
 
 -- describir qué hace la función: .....
 publicacionesQueLeGustanA :: RedSocial -> Usuario -> [Publicacion]
-publicacionesQueLeGustanA = undefined
+publicacionesQueLeGustanA (us, rs, (p:ps)) user | longitud ps == 0 && not (pertenece user (likesDePublicacion p)) = []  
+                                                | longitud ps == 0 && pertenece user (likesDePublicacion p) = [p]
+                                                | pertenece user (likesDePublicacion p) = p : publicacionesQueLeGustanA (us, rs, ps) user
+                                                | otherwise                             = publicacionesQueLeGustanA (us, rs, ps) user  
 
 -- describir qué hace la función: .....
 lesGustanLasMismasPublicaciones :: RedSocial -> Usuario -> Usuario -> Bool
-lesGustanLasMismasPublicaciones = undefined
+lesGustanLasMismasPublicaciones rs u1 u2 = publicacionesQueLeGustanA rs u1 == publicacionesQueLeGustanA rs u2
 
 -- describir qué hace la función: .....
+
+likeoTodas :: [Publicacion] -> Usuario -> Bool
+likeoTodas [p] user    = pertenece user (likesDePublicacion p)  
+likeoTodas (p:ps) user = pertenece user (likesDePublicacion p) && likeoTodas ps user
+
 tieneUnSeguidorFiel :: RedSocial -> Usuario -> Bool
-tieneUnSeguidorFiel = undefined
+tieneUnSeguidorFiel ((u:us), rs, ps) user | longitud us == 0 && likeoTodas (publicacionesDe red user) u = True
+                                          | longitud us == 0 && not (likeoTodas (publicacionesDe red user) u) = False
+                                          | likeoTodas (publicacionesDe red user) u = True
+                                          | otherwise = tieneUnSeguidorFiel (us, rs, ps) user 
+                                          where red = ((u:us), rs, ps)
+
+
+
 
 -- describir qué hace la función: .....
+
+secuenciaDeAmigos :: RedSocial -> Usuario -> Usuario -> [Relacion]
+secuenciaDeAmigos ((u:us), rs, ps) u1 u2 | longitud us == 0 && pertenece u (amigosDe red u1) = [(u1, u)]
+                                         | longitud us == 0 && not (pertenece u (amigosDe red u1)) = []
+                                         | pertenece u (amigosDe red u1) = (u1, u) : secuenciaDeAmigos (us, rs, ps) u u2 
+                                         | not (pertenece u (amigosDe red u1)) = secuenciaDeAmigos (us, rs, ps) u1 u2
+                                         where red = ((u:us), rs, ps)
 existeSecuenciaDeAmigos :: RedSocial -> Usuario -> Usuario -> Bool
-existeSecuenciaDeAmigos = undefined
+existeSecuenciaDeAmigos ((u:us), rs, ps) u1 u2 | pertenece u (amigosDe red u1) && pertenece u (amigosDe red u2) = True
+                                               | pertenece u (amigosDe red u1) = False
+                                               where red = ((u:us), rs, ps)
